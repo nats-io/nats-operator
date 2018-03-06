d18a82b56dc1b80e84c52210cca236af1470ae33
Waldemar Quevedo <wally@synadia.com>
2018-03-06 11:29:10 -0800

    Move boilerplate to under hack

--- hack/codegen.sh
+++ hack/codegen.sh
@@ -4,9 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-echo "--- Updating NatsCluster CRD Deepcopy"
+echo "--- Updating NatsCluster CRD Deepcopy..."
 deepcopy-gen --logtostderr -v=1 \
              --input-dirs="github.com/nats-io/nats-operator/pkg/spec" \
 	     --output-file-base zz_generated.deepcopy  \
 	     --bounding-dirs "github.com/nats-io/nats-operator/pkg/spec" \
-             --go-header-file boilerplate.txt
+             --go-header-file hack/boilerplate.txt
