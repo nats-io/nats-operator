7fd2f2b9dae1fd6d9014f5fd11c32d471744225f
Julian Dolce <dolce.julian@gmail.com>
2020-05-15 22:24:59 -0400

    use correct binary path when entering lame duck mode. (#260)
    This patch will ensure that when upgrading from 1.4.1 to 2.x.x, currently running 1.4.1 servers are properly
    placed into lame duck mode.
    
    Co-authored-by: Waldemar Quevedo <wally@synadia.com>

--- pkg/cluster/reconcile.go
+++ pkg/cluster/reconcile.go
@@ -45,7 +45,7 @@ func (c *Cluster) reconcileSize() error {
 		// Remove extra pods as required in order to meet the desired size.
 		// As we remove each pod, we must update the config secret so that routes are re-computed.
 		for idx := currentSize - 1; idx >= desiredSize; idx-- {
-			if err := c.tryGracefulPodDeletion(pods[idx], c.cluster.Spec.Version); err != nil {
+			if err := c.tryGracefulPodDeletion(pods[idx]); err != nil {
 				return err
 			}
 			if err := c.updateConfigSecret(); err != nil {
@@ -94,7 +94,7 @@ func (c *Cluster) reconcileVersion() error {
 		for _, pod := range pods {
 			if kubernetesutil.GetNATSVersion(pod) != c.cluster.Spec.Version {
 				c.maybeUpgradeMgmtService()
-				return c.upgradePod(pod, desiredVersion)
+				return c.upgradePod(pod)
 			}
 		}
 	}
