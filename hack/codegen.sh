93fa110a5209f7dea5a3de564a7993aeb9096fd5
Derek Wang <whynowy@gmail.com>
2020-05-22 15:45:00 -0700

    Improve codegen performance

--- hack/codegen.sh
+++ hack/codegen.sh
@@ -6,38 +6,25 @@ set -o pipefail
 
 SCRIPT_ROOT=$(realpath $(dirname ${BASH_SOURCE})/..)
 
-# Grab code-generator version from go.sum.
-CODEGEN_VERSION=$(grep 'k8s.io/code-generator' go.sum | awk '{print $2}' | head -1)
-CODEGEN_PKG=$(echo `go env GOPATH`"/pkg/mod/k8s.io/code-generator@${CODEGEN_VERSION}")
+if [ ! -d "${SCRIPT_ROOT}/vendor" ]; then
+  go mod vendor
+fi
 
-echo ">> Using ${CODEGEN_PKG}"
+FAKE_GOPATH="$(mktemp -d)"
+trap 'rm -rf ${FAKE_GOPATH}' EXIT
+FAKE_REPOPATH="${FAKE_GOPATH}/src/github.com/nats-io/nats-operator"
+mkdir -p "$(dirname "${FAKE_REPOPATH}")" && ln -s "${SCRIPT_ROOT}" "${FAKE_REPOPATH}"
 
-# code-generator does work with go.mod but makes assumptions about
-# the project living in `$GOPATH/src`. To work around this and support
-# any location; create a temporary directory, use this as an output
-# base, and copy everything back once generated.
-TEMP_DIR=$(mktemp -d)
-cleanup() {
-    echo ">> Removing ${TEMP_DIR}"
-    rm -rf ${TEMP_DIR}
-}
-trap "cleanup" EXIT SIGINT
+export GOPATH="${FAKE_GOPATH}"
+export GO111MODULE="off"
 
-echo ">> Temporary output directory ${TEMP_DIR}"
+cd "${FAKE_REPOPATH}"
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${FAKE_REPOPATH}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
-# Ensure we can execute.
-chmod +x ${CODEGEN_PKG}/generate-groups.sh
-
-# HACK_DIR is the path to the current directory.
-HACK_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-# ROOT_PKG is the name of the root package.
-ROOT_PKG="github.com/nats-io/nats-operator"
-
-${CODEGEN_PKG}/generate-groups.sh all \
+bash -x ${CODEGEN_PKG}/generate-groups.sh all \
     github.com/nats-io/nats-operator/pkg/client github.com/nats-io/nats-operator/pkg/apis \
     "nats:v1alpha2" \
-    --output-base "${TEMP_DIR}" \
     --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.txt
 
-# Copy everything back.
-cp -r "${TEMP_DIR}/github.com/nats-io/nats-operator/." "${SCRIPT_ROOT}/"
+rm -rf vendor
+
